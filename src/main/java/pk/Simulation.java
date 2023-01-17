a691b209cc8f02cef7758cbc8546d362d021439f
Sceptres <abdalq2003@gmail.com>
2023-01-17 11:51:38 -0500

    Implement method to run the simulation

--- src/main/java/pk/Simulation.java
+++ src/main/java/pk/Simulation.java
@@ -3,6 +3,10 @@ package pk;
 import pk.game.Game;
 
 public class Simulation {
+
+    // The number of simulations to run
+    public static final int NUM_SIMS = 42;
+
     // Class fields
     private final Game game;
 
@@ -16,5 +20,17 @@ public class Simulation {
      */
     public Game getGame() {
         return this.game;
+    }
+
+    /**
+     * Run the simulation
+     */
+    public void run() {
+        // Simulate games
+        for(int i=0; i < Simulation.NUM_SIMS; i++) {
+            // Simulate the game
+            this.game.play();
+            this.game.reset();
+        }
     }
 }
