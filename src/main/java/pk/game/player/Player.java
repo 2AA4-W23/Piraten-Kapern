87a900e245e1b1bb3203e83c8e2799992c77c802
Sceptres <abdalq2003@gmail.com>
2023-01-16 19:37:08 -0500

    Add critical fields for players Add ScoreCard to keep track of score Add Dice so player can roll during their turn Add win Counter so the player can keep track of their wins during a game

--- src/main/java/pk/game/player/Player.java
+++ src/main/java/pk/game/player/Player.java
@@ -1,9 +1,19 @@
 package pk.game.player;
 
+import pk.game.count.Counter;
+import pk.game.dice.Dice;
+import pk.game.score.ScoreCard;
+
 public class Player {
 
-    public Player() {
+    private final ScoreCard scoreCard;
+    private final Dice dice;
+    private final Counter wins;
 
+    public Player() {
+        this.scoreCard = new ScoreCard();
+        this.dice = new Dice();
+        this.wins = new Counter();
     }
 
 }
