5c22fa40371201f3618b41e3ab7154e4523f49e7
Sceptres <abdalq2003@gmail.com>
2023-01-17 12:01:30 -0500

    Implement method to display simulation statistics

--- src/main/java/pk/Simulation.java
+++ src/main/java/pk/Simulation.java
@@ -33,4 +33,19 @@ public class Simulation {
             this.game.reset();
         }
     }
+
+    /**
+     * Display the statistics of the simulation
+     */
+    public void displayStats() {
+        // Calculate win statistics for both players
+        int p1Wins = this.getGame().getPlayer1().getWins().getCount();
+        int p2Wins = this.getGame().getPlayer2().getWins().getCount();
+        double p1WinRate = p1Wins/(double)Simulation.NUM_SIMS;
+        double p2WinRate = p2Wins/(double)Simulation.NUM_SIMS;
+
+        // Print game results
+        System.out.printf("Player 1 won %%%.2f of the games!\n", p1WinRate*100);
+        System.out.printf("Player 2 won %%%.2f of the games!\n", p2WinRate*100);
+    }
 }
