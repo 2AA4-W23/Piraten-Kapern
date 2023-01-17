92802052ce848c05cae8b2786a1de29df07c18c4
Sceptres <abdalq2003@gmail.com>
2023-01-17 11:49:59 -0500

    Basic simulation class Will be used to handle the simulation of the games

---
+++ src/main/java/pk/Simulation.java
@@ -0,0 +1,12 @@
+package pk;
+
+import pk.game.Game;
+
+public class Simulation {
+    // Class fields
+    private final Game game;
+
+    public Simulation() {
+        this.game = new Game();
+    }
+}
