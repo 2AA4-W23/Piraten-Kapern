1e9efc5d73b9713b496ca801b20c5790b8255497
Sceptres <abdalq2003@gmail.com>
2023-01-16 20:53:45 -0500

    Add isTurnOver field to player class Used to keep track of whether the players turn is currently over or not

--- src/main/java/pk/game/player/Player.java
+++ src/main/java/pk/game/player/Player.java
@@ -10,12 +10,14 @@ public class Player {
     private final Dice dice;
     private final Counter wins;
     private final Counter turnsPlayed;
+    private boolean isTurnOver;
 
     public Player() {
         this.scoreCard = new ScoreCard();
         this.dice = new Dice();
         this.wins = new Counter();
         this.turnsPlayed = new Counter();
+        this.isTurnOver = false;
     }
 
     /**
@@ -48,5 +50,21 @@ public class Player {
      */
     public Counter getTurnsPlayed() {
         return this.turnsPlayed;
+    }
+
+    /**
+     *
+     * @return Is the players turn over?
+     */
+    public boolean isTurnOver() {
+        return this.isTurnOver;
+    }
+
+    /**
+     *
+     * @param turnOver The new value indicating whether the players turn is over or not
+     */
+    public void setTurnOver(boolean turnOver) {
+        this.isTurnOver = turnOver;
     }
 }
