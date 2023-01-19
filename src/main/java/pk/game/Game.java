44fd7fe4b4710e6d7bc82506415849fa0d90c27a
Sceptres <abdalq2003@gmail.com>
2023-01-19 12:29:01 -0500

    Fix game winning logic

--- src/main/java/pk/game/Game.java
+++ src/main/java/pk/game/Game.java
@@ -45,32 +45,29 @@ public class Game {
         do {
             // Players play their turns
             this.getPlayer1().play();
+            if(this.getPlayer1().getScoreCard().totalScore() >= GameScoreCard.WIN_SCORE) { // Player 1 reached 6k points?
+                // Give player 2 an extra turn
+                this.getPlayer2().play();
+                break;
+            }
+
             this.getPlayer2().play();
-
-            // Get the scores of the players
-            int p1Score = this.p1.getScoreCard().totalScore();
-            int p2Score = this.p2.getScoreCard().totalScore();
-
-            // Find who won
-            if(p1Score >= GameScoreCard.WIN_SCORE || p2Score >= GameScoreCard.WIN_SCORE) {
-                // Find player who gets extra turn
-                if(p1Score >= GameScoreCard.WIN_SCORE) {
-                    this.getPlayer2().play();
-                } else {
-                    this.getPlayer1().play();
-                }
-
-                if(p1Score > p2Score) { // Did player p1 win?
-                    this.getPlayer1().getWins().add(1);
-                } else if(p1Score < p2Score) { // Did player p2 win?
-                    this.getPlayer2().getWins().add(1);
-                }
-
-                // Otherwise, the score of both players are equal it is a draw. Game ends as a tie.
-
+            if(this.getPlayer2().getScoreCard().totalScore() >= GameScoreCard.WIN_SCORE) { // Player 2 reached 6k points?
+                // Give player 1 an extra turn
+                this.getPlayer1().play();
                 break;
             }
         } while(true);
+
+        // Get the scores of the players
+        int p1Score = this.getPlayer1().getScoreCard().totalScore();
+        int p2Score = this.getPlayer2().getScoreCard().totalScore();
+
+        if(p1Score > p2Score) { // Did player p1 win?
+            this.getPlayer1().getWins().add(1);
+        } else if(p1Score < p2Score) { // Did player p2 win?
+            this.getPlayer2().getWins().add(1);
+        }
     }
 
 }
