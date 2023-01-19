a1872c393b6fa1b741b277fcf824a1bf4c262f42
Sceptres <abdalq2003@gmail.com>
2023-01-18 22:19:12 -0500

    DiceHolder holds 8 dice

--- src/main/java/pk/game/dice/holder/DiceHolder.java
+++ src/main/java/pk/game/dice/holder/DiceHolder.java
@@ -1,17 +1,17 @@
 package pk.game.dice.holder;
 
 import pk.game.dice.Dice;
-import pk.game.score.scorable.Faces;
+
+import java.util.Arrays;
 
 public class DiceHolder {
 
     // Class fields
-    private final Dice dice;
-    private final Faces[] faces;
+    private final Dice[] dice;
 
     public DiceHolder() {
-        this.dice = new Dice();
-        this.faces = new Faces[Dice.MAX_DICE];
+        this.dice = new Dice[Dice.MAX_DICE];
+        Arrays.fill(this.dice, new Dice()); // Fill dice array with 8 dice
     }
 
 }
