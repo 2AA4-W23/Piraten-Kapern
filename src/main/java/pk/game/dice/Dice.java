fdb82c16c106f76987e77634128decc25f0086ef
Sceptres <abdalq2003@gmail.com>
2023-01-18 22:14:38 -0500

    Allow dice to keep track of rolled face

--- src/main/java/pk/game/dice/Dice.java
+++ src/main/java/pk/game/dice/Dice.java
@@ -1,6 +1,7 @@
 package pk.game.dice;
 import pk.game.score.scorable.Faces;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Dice {
@@ -8,10 +9,38 @@ public class Dice {
     public static final int MIN_DICE = 2; // Minimum number of dice a player can roll
     public static final int MAX_DICE = 8; // Maximum number of dice a player can roll
 
-    public Faces roll() {
+    private Faces face;
+
+    public Dice() {
+        this.face = null;
+    }
+
+    /**
+     *
+     * @return The {@link Faces} this dice is currently on
+     */
+    public Faces getFace() {
+        if(Objects.isNull(this.face))
+            throw new IllegalStateException("Dice must be rolled first!");
+
+        return this.face;
+    }
+
+    /**
+     *
+     * @param face The {@link Faces} to set the face of this dice to
+     */
+    public void setFace(Faces face) {
+        this.face = face;
+    }
+
+    /**
+     * Roll this {@link Dice}
+     */
+    public void roll() {
         int howManyFaces = Faces.values().length;
         Random bag = new Random();
-        return Faces.values()[bag.nextInt(howManyFaces)];
+        this.setFace(Faces.values()[bag.nextInt(howManyFaces)]);
     }
 
     /**
@@ -23,7 +52,8 @@ public class Dice {
         Faces[] rollResult = new Faces[n];
 
         for(int i=0; i < n; i++) {
-            rollResult[i] = this.roll();
+            this.roll();
+            rollResult[i] = this.getFace();
         }
 
         return rollResult;
