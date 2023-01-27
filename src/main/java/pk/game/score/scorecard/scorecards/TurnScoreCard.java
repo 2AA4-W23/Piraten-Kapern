278978d23b7d3add0b4b93314ea911b0aa644524
Sceptres <abdalq2003@gmail.com>
2023-01-27 09:06:28 -0500

    Implement reserve in turn scorecard This reserve will be used to store a single scorable that will always be added to a players roll during this turn

--- src/main/java/pk/game/score/scorecard/scorecards/TurnScoreCard.java
+++ src/main/java/pk/game/score/scorecard/scorecards/TurnScoreCard.java
@@ -10,6 +10,29 @@ import java.util.Map;
 import java.util.Objects;
 
 public class TurnScoreCard extends AbstractScoreCard {
+
+    private Scorable reserve;
+
+    public TurnScoreCard() {
+        this.reserve = null;
+    }
+
+    /**
+     *
+     * @return {@link TurnScoreCard#reserve}. This is a {@link Scorable} instance that will be added to the player roll
+     */
+    public Scorable getReserve() {
+        return this.reserve;
+    }
+
+    /**
+     *
+     * @param reserve The {@link Scorable} to set {@link TurnScoreCard#reserve} to
+     */
+    public void putReserve(Scorable reserve) {
+        this.reserve = reserve;
+    }
+
     /**
      *
      * @param map The map of the scorables to add and their counts
@@ -18,6 +41,10 @@ public class TurnScoreCard extends AbstractScoreCard {
         this.clear(); // Clear old scores from last roll
 
         super.getScoreCount().putAll(map); // Add new scores
+
+        if(Objects.nonNull(this.getReserve())) // Does the player have a reserved scorable this turn?
+            super.addScore(this.getReserve(), 1);
+
         this.addCombos();
 
         if(GameRules.isBonusChestRoll(this)) { // Is there a bonus chest in the scorecard?
@@ -38,5 +65,11 @@ public class TurnScoreCard extends AbstractScoreCard {
                 this.addScore(group, 1);
             }
         });
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+        this.putReserve(null);
     }
 }
