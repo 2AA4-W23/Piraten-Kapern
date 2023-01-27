98e2f43453841424a3dc568b05a37350f071a8da
Sceptres <abdalq2003@gmail.com>
2023-01-26 22:56:53 -0500

    Implement use method of monkey business card Card removes any existing combination of parrots or monkeys and adds them up to make 1 combination

--- src/main/java/pk/game/cards/MonkeyBusinessCard.java
+++ src/main/java/pk/game/cards/MonkeyBusinessCard.java
@@ -1,6 +1,12 @@
 package pk.game.cards;
 
+import pk.game.GameRules;
+import pk.game.player.Player;
+import pk.game.score.scorable.Faces;
+import pk.game.score.scorable.Groups;
 import pk.game.strategy.player.PlayerStrategy;
+
+import java.util.Objects;
 
 public class MonkeyBusinessCard extends AbstractCard {
 
@@ -13,5 +19,33 @@ public class MonkeyBusinessCard extends AbstractCard {
     @Override
     public PlayerStrategy getStrategy() {
         return null;
+    }
+
+    @Override
+    public void use(Player player) {
+        super.use(player);
+
+        // Calculate the number of monkey and parrots rolled
+        Integer monkeyCount = player.getTurnScoreCard().getScore(Faces.MONKEY);
+        Integer parrotCount = player.getTurnScoreCard().getScore(Faces.PARROT);
+        Integer monkeyParrotSum = monkeyCount + parrotCount;
+
+        // Get the combination of the monkey and parrots
+        Groups monkeyParrotCombo = GameRules.GROUP_MAP.get(monkeyParrotSum);
+
+        if (Objects.nonNull(monkeyParrotCombo)) { // Does the player have enough monkeys and parrots to make a combination?
+            // Monkey combo and parrot combo
+            Groups monkeyCombo = GameRules.GROUP_MAP.get(monkeyCount);
+            Groups parrotCombo = GameRules.GROUP_MAP.get(parrotCount);
+
+            if(Objects.nonNull(monkeyCombo)) // Does the player have a combo with monkeys?
+                player.getTurnScoreCard().removeScore(monkeyCombo, 1);
+
+            if(Objects.nonNull(parrotCombo)) // Does the player have a combo with parrots?
+                player.getTurnScoreCard().removeScore(parrotCombo, 1);
+
+            // Add combo of monkeys and parrots combined
+            player.getTurnScoreCard().addScore(monkeyParrotCombo, 1);
+        }
     }
 }
