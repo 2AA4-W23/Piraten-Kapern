5f9a416108f596d28b1f8ab088f5229ee118f2ad
Sceptres <abdalq2003@gmail.com>
2023-01-28 22:03:44 -0500

    Add scorecard interface

---
+++ src/main/java/pk/game/score/scorecard/ScoreCard.java
@@ -0,0 +1,15 @@
+package pk.game.score.scorecard;
+
+public interface ScoreCard {
+
+    /**
+     * Clear the {@link ScoreCard}
+     */
+    void clear();
+    /**
+     *
+     * @return The total score found in this scorecard
+     */
+    int totalScore();
+
+}
