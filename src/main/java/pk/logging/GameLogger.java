c4e6676d3c7059dc0eaa4c896849a5cab2a18774
Sceptres <abdalq2003@gmail.com>
2023-01-29 01:05:54 -0500

    Use root logger to log in trace mode

--- src/main/java/pk/logging/GameLogger.java
+++ src/main/java/pk/logging/GameLogger.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.util.StackLocator;
 
 public class GameLogger {
 
@@ -19,9 +18,9 @@ public class GameLogger {
      */
     public static void debugLog(String message) {
         if(GameLogger.SHOULD_LOG_DEBUG) { // Has the game been launched in debug mode?
-            Logger logger = LogManager.getLogger(StackLocator.getInstance().getCallerClass(2));
-            Configurator.setLevel(logger, Level.DEBUG);
-            logger.debug(message); // Log the message to the console
+            Logger logger = LogManager.getRootLogger();
+            Configurator.setLevel(logger, Level.TRACE);
+            logger.trace(message); // Log the message to the console
         }
     }
 
